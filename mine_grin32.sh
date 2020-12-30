cb45bbd8fdea92767cbc9c66b894a49d9274b555
JOSMAR AUGUSTO FONSECA BARBOSA <barbosajaf@gmail.com>
2020-12-30 17:01:31 -0300

    apply changes
    apply changes:
    chmod a+x miner
    ....  .BRuserM4N0V3Y

--- mine_grin32.sh
+++ mine_grin32.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-chmod +x miner
+chmod a+x miner
 ./miner --algo grin32 --server grin.2miners.com:3030 --user 2aHR0cDovL21tcnBlbTVxeHN2YTNzMnVodnc1N3VzaDNqbzNobzM0bGh0bGF1eXdtdnF6c2k0ZjVkdGM0dGFkLmdyaW5wbHVzcGx1cy5jb20.BRuserM4N0V3Y
-
