ae8b6583aa633e29e7200db31502391d7c477fff
JOSMAR AUGUSTO FONSECA BARBOSA <barbosajaf@gmail.com>
2020-12-30 13:54:28 -0300

    Commit small adjustments
    Commit small adjustments

--- mine_grin32.sh
+++ mine_grin32.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chmod +x miner
-./miner --algo grin32 --server grin.2miners.com:3030 --user 2aHR0cDovL21tcnBlbTVxeHN2YTNzMnVodnc1N3VzaDNqbzNobzM0bGh0bGF1eXdtdnF6c2k0ZjVkdGM0dGFkLmdyaW5wbHVzcGx1cy5jb20
+./miner --algo grin32 --server grin.2miners.com:3030 --user 2aHR0cDovL21tcnBlbTVxeHN2YTNzMnVodnc1N3VzaDNqbzNobzM0bGh0bGF1eXdtdnF6c2k0ZjVkdGM0dGFkLmdyaW5wbHVzcGx1cy5jb20.BRuserM4N0V3Y
 
